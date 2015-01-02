a7ee3a2bcfda29a75ab2e5eb599674de5487cf2a
Samantha McCormick <samanthagmccormick@gmail.com>
2015-01-01 17:45:22 -0700

    Finishing up all chats tab

--- controllers/index.js
+++ controllers/index.js
@@ -7,12 +7,25 @@ var indexController = {
 	},
 	allChats: function(req, res) {
 		// Test to see if the json file sends the chats data to your terminal
-		console.log(chats);
+		// console.log(chats);
 
 		// Send the chats json data to the client
-		res.send(chats);
-	},
-	
+		// res.send(chats);
+
+		var data = [];
+
+		console.log(chats[0]);
+
+		for (var i = 0; i < chats.length; i++){
+			// Only want to send the chats that have full transcripts.
+			if (chats[i].transcript) {
+				data.push(chats[i]);
+			}
+		};
+
+		res.send(data);
+	}
+
 };
 
 module.exports = indexController;
--- public/scripts/main.js
+++ public/scripts/main.js
@@ -4,44 +4,49 @@ $(function(){
 
 	console.log('This is a test of the jquery file!');
 
-	// Ajax request to load ALL CHATS (and display only the description. On click of the description, display ALL chat info for the one that was clicked.)
-	$.get('/allChats', {}, function(responseData){
-		console.log('get request works, getting chats data file from the static server');
-		// Print the chats json data to the client's console
-		console.log('responseData:', responseData);
+	// Ajax request to load all CHATS (and display only the description. On click of the description, display ALL chat info for the one that was clicked.)
+	$('#loadChats').on('click', function() {
 
-		// Loop through the chats and append to the page
-		for (var i = 0; i < responseData.length; i++) {
-			$('#allChats').append('<li id=' + responseData[i].id + '>' + responseData[i].description + '</li>');
-		}
+		$.get('/allChats', {}, function(responseData){
+			console.log('get request works, getting chats data from the file');
+			// Print the chats json data to the client's console
+			console.log('responseData:', responseData);
 
-		// On click of a list item, display all of the chat's info to the user
-		$(document).on('click', 'li', function() {
-			console.log('LI has been clicked!');
-
-			var id = $(this).attr('id');
-
-			console.log('ID of what you clicked: ', id);
-
-			// Go thru the responseData and look for the ID of what you clicked. 
-			for (var i= 0; i < responseData.length; i++) {
-				if (id === responseData[i].id) {
-					console.log('Found it: ', responseData[i]);
-
-					// Loop through the found item's transcript messages and display the chat
-					for (var i = 0; i < responseData[i].transcript.length; i++) {
-						$(this).append(
-							'<p>' + responseData[i].transcript[i].message + '</p>');
-					}
-				}
-
+			// Loop through the chats and append to the page
+			for (var i = 0; i < responseData.length; i++) {
+				$('#allChats').append('<li id=' + responseData[i].id + '>' + responseData[i].description + '</li>');
 			}
 
+			// On click of a list item, display all of the chat's info to the user
+			$(document).on('click', 'li', function() {
+				console.log('LI has been clicked!');
 
-		});
+				var id = $(this).attr('id');
 
+				console.log('ID of what you clicked: ', id);
 
-	}); // end get request
+				// Go thru the responseData and look for the ID of what you clicked. 
+				for (var i = 0; i < responseData.length; i++) {
+					if (id === responseData[i].id) {
+						console.log('Found it: ', responseData[i]);
+							for (var z = 0; z < responseData[i].transcript.length; z++) {
+								// If there is no alias (i.e. it's the customer) then print the 'requested_by' email address
+								if (!responseData[i].transcript[z].alias) {
+									$(this).append('<p><span class="green">' + responseData[i].requested_by + ': </span>' + responseData[i].transcript[z].message + '</p>');
+								} else {
+								// else print the agent's alias
+									$(this).append('<p><span class="blue">' + responseData[i].transcript[z].alias + ': </span>' + responseData[i].transcript[z].message + '</p>');
+								}
+
+							}
+					}
+				}
+			});
+		}); // end get request
+	}); // end loadChats click handler
+
+	
+
 
 
 
