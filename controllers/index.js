fadfeb562d704a149fc730fa8372f9507cc8fe3a
Samantha McCormick <samanthagmccormick@gmail.com>
2015-01-09 17:20:02 -0700

    API

--- controllers/index.js
+++ controllers/index.js
@@ -12,12 +12,14 @@ var indexController = {
 		// console.log('API data: ', req.body);
 		console.log('GET request test: ', req.body);
 		console.log('GET request test with res.body ', res.body);
-		res.send(200);
+		res.send(req.body);
+		res.render('index');
 		res.json({message: "yes! welcome to the API"});
 
 	},
 	apiPost: function(req, res) {
-		res.send(200);
+		res.send(req.body);
+		res.render('index');
 		console.log('POST request test: ', req.body);
 		console.log('POST request test with res.body ', res.body);
 
